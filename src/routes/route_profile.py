85b5b689aaa22c779421ba9eeabcb8d16b4f3acc
Kartik Iyyer <kartikiyyer@gmail.com>
2024-02-18 22:09:53 -0800

    Added more project descriptions

--- src/routes/route_profile.py
+++ src/routes/route_profile.py
@@ -13,5 +13,13 @@ def index():
 def project_policy_enforcer():
 	return render_template('policy-enforcer.html')
 
+@app.route('/project/cloud-native-contrail-networking')
+def project_cloud_native_contrail_networking():
+	return render_template('cloud-native-contrail-networking.html')
+
+@app.route('/project/juniper-fabric-manager')
+def project_juniper_fabric_manager():
+	return render_template('juniper-fabric-manager.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
